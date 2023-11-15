e6f3f04af3c3738c7ef8c2af6699e1d57607d62f
Filipe Carneiro <fcarneiro@doutorpc.net>
2023-11-15 09:32:10 +0000

    fix: EnumHelper with null

--- Extensions/EnumHelper.cs
+++ Extensions/EnumHelper.cs
@@ -6,15 +6,18 @@ namespace ELabel.Extensions
 {
     public static class EnumHelper
     {
-        public static string? GetDisplayName(Enum enumValue)
+        public static string? GetDisplayName(Enum? enumValue)
         {
+            if (enumValue == null)
+                return null;
+
             return enumValue?.GetType()?
            .GetMember(enumValue.ToString())?[0]?
            .GetCustomAttribute<DisplayAttribute>()?
            .Name;
         }
 
-        public static string? GetDisplayDescription(Enum enumValue)
+        public static string? GetDisplayDescription(Enum? enumValue)
         {
             if (enumValue == null)
                 return null;
