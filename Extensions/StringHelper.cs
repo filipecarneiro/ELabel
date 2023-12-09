fac849139e52cf9bc1d76e45ec509c2571f943ba
Filipe Carneiro <fcarneiro@doutorpc.net>
2023-12-09 15:44:35 +0000

    Language menu with capitalized names

---
+++ Extensions/StringHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace ELabel.Extensions
+{
+    public static class StringHelper
+    {
+        public static string FirstCharToUpper(this string input) =>
+            input switch
+            {
+                null => throw new ArgumentNullException(nameof(input)),
+                "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+                _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
+            };
+    }
+}
