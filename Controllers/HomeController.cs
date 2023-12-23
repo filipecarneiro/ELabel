c504d48d1254e457c9fc17de9b2d99d14653ef9b
Filipe Carneiro <fcarneiro@doutorpc.net>
2023-12-23 10:00:22 +0000

    chore: System Info

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,10 +10,12 @@ namespace ELabel.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
@@ -23,6 +25,26 @@ namespace ELabel.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Info()
+        {
+            ViewData["KnownProxy"] = _configuration.GetValue<string>("KnownProxy");
+
+            ViewData["RemoteIpAddress"] = HttpContext.Connection.RemoteIpAddress?.ToString();
+
+            System.Text.StringBuilder headers = new();
+            foreach (var header in HttpContext.Request.Headers)
+            {
+                headers.Append(header.Key);
+                headers.Append('=');
+                headers.AppendLine(header.Value);
+            }
+
+            ViewData["Headers"] = headers.ToString();
+
             return View();
         }
 
