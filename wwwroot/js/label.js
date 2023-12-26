03b425aaaa1a1489f1ff417f3c2313b5402ec72b
Filipe Carneiro <fcarneiro@doutorpc.net>
2023-12-26 11:41:00 +0000

    security: stricter Content Security Policy (CSP)

---
+++ wwwroot/js/label.js
@@ -0,0 +1,32 @@
+﻿// Select Theme
+document.addEventListener('DOMContentLoaded', function () {
+    var htmlElement = document.documentElement;
+    var checkSwitchTheme = document.getElementById('checkSwitchTheme');
+    var btnDarkTheme = document.getElementById('btnDarkTheme');
+    var btnLightTheme = document.getElementById('btnLightTheme');
+    checkSwitchTheme.addEventListener('click', function () {
+        if (checkSwitchTheme.checked) {
+            htmlElement.setAttribute('data-bs-theme', 'light');
+        }
+        else {
+            htmlElement.setAttribute('data-bs-theme', 'dark');
+        }
+    });
+    btnDarkTheme.addEventListener('click', function () {
+        checkSwitchTheme.checked = false;
+        htmlElement.setAttribute('data-bs-theme', 'dark');
+    });
+    btnLightTheme.addEventListener('click', function () {
+        checkSwitchTheme.checked = true;
+        htmlElement.setAttribute('data-bs-theme', 'light');
+    });
+});
+
+// Select Language
+document.addEventListener('DOMContentLoaded', function () {
+    var languageSelect = document.getElementById('languageSelect');
+    var selectLanguage = document.getElementById('selectLanguage');
+    languageSelect.onchange = function () {
+        selectLanguage.submit();
+    };
+});
--- wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿(function() {
-  const selectElement = $("#selectLanguage select");
-  // Attach event listener to the select element
-  selectElement.on("change", function() {
-    // Submit the parent form
-    selectElement.parent().trigger("submit");
-  });
-})();
